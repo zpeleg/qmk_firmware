f1e42efbf15d2bce391aacbfdda68e65864f24cc
Zach White <skullydazed@gmail.com>
2020-11-02 23:09:43 -0800

    more california cleanup

--- keyboards/clueboard/california/config.h
+++ keyboards/clueboard/california/config.h
@@ -1,9 +1,3 @@
 #pragma once
 
 #include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define PRODUCT_ID      0x23B0
-#define MANUFACTURER    Clueboard
-#define PRODUCT         California Macropad
-#define DESCRIPTION     A 10-key macropad shaped like California
