295c653e05912bc2d68a625efa31827d546d8437
Ziv Peleg <zivpeleg@gmail.com>
2020-11-11 01:13:08 +0200

    Fix colemak layer

--- keyboards/atreus44/keymaps/default/keymap.c
+++ keyboards/atreus44/keymaps/default/keymap.c
@@ -8,22 +8,23 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QW 0
-#define _RS 1
-#define _LW 2
-#define _CM 3
+#define _CM 1
+#define _RS 2
+#define _LW 3
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RESET,   KC_N,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_ESC, KC_LCTL, KC_LGUI,  KC_LALT, KC_LSFT,  KC_BSPC, KC_TAB, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
- [_CM] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN    ,
+ [_CM] = LAYOUT( /* Colemak */
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN   ,
     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O ,
     KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    RESET,   KC_N,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_ESC, KC_LCTL, KC_LGUI,  KC_LALT, KC_LSFT,  KC_BSPC, KC_TAB, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -35,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR, KC_TRNS, KC_TRNS, KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+    MO(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
   /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
+   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10  
    *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
    *       volup             reset      ||             F1    F2    F3   F12
    *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
@@ -46,5 +47,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   DF(_QW), DF(_CM), KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
+    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS )
 };
