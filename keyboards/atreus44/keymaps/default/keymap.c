a0566d62f9e7c344b598616fcc1472648dd13849
Ziv Peleg <zivpeleg@gmail.com>
2020-11-28 21:25:47 +0200

    fix formatting

--- keyboards/atreus44/keymaps/default/keymap.c
+++ keyboards/atreus44/keymaps/default/keymap.c
@@ -15,16 +15,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RESET,   KC_N,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    LT(_LW, KC_ESC), KC_LCTL, KC_LGUI,  KC_LALT, KC_LSFT,  KC_BSPC, KC_TAB, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RESET,   KC_N,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+    KC_ESC, KC_LCTL, KC_LGUI,  KC_LALT, KC_LSFT,  KC_BSPC, LT(_LW, KC_TAB), KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
  [_CM] = LAYOUT( /* Colemak */
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN   ,
-    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O ,
-    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    RESET,   KC_N,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH ,
-    LT(_LW, KC_ESC), KC_LCTL, KC_LGUI,  KC_LALT, KC_LSFT,  KC_BSPC, KC_TAB, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN   ,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                KC_M,    KC_N,    KC_E,    KC_I,    KC_O ,
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    RESET,   KC_N,              KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH ,
+    KC_ESC, KC_LCTL, KC_LGUI,  KC_LALT, KC_LSFT,  KC_BSPC, LT(_LW, KC_TAB), KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
