9fdb5ad3e07281e36861d4f9f69512254af721b0
Ziv Peleg <zivpeleg@gmail.com>
2021-01-06 00:47:49 +0200

    Many bunched changes, not sure exactly what

--- keyboards/lily58/keymaps/zpeleg/config.h
+++ keyboards/lily58/keymaps/zpeleg/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 160
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
--- keyboards/lily58/keymaps/zpeleg/keymap.c
+++ keyboards/lily58/keymaps/zpeleg/keymap.c
@@ -15,21 +15,31 @@ enum layer_number {
   _LOWER,
   _RAISE,
   _ADJUST,
+  _SHORTCUTS
 };
 
-#define ___________NAV_UPPER______________ KC_HOME,   KC_UP,  KC_END, KC_PGUP
-#define ___________NAV_LOWER______________ KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN
+enum custom_keycodes {
+    SMILE = SAFE_RANGE,
+    SMILE_HEB,
+    SHRUG,
+    TFLIP,
+    POOP,
+    LAUNCH_TERMINAL
+};
 
+// these don't work very good...
+#define CHANGE_LANG_ENG LSFT(LALT(KC_0))
+#define CHANGE_LANG_HEB LSFT(LALT(KC_1))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_LALT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT), \
-                 KC_LGUI, MO(_RAISE), KC_LCTRL, LSFT_T(KC_ENT), LCTL_T(KC_SPC), KC_BSPC, MO(_LOWER), KC_DEL \
+  KC_ESC,   KC_1,                   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
+  KC_TAB,   KC_Q,                   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+  KC_LALT,  KC_A,                   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LT(_SHORTCUTS, KC_LBRC),  LCTL_T(KC_RBRC),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT(KC_LALT), \
+                 KC_LGUI, MO(_RAISE), KC_LCTRL, LSFT_T(KC_ENT), KC_SPC, KC_BSPC, MO(_LOWER), KC_DEL \
 ),
 [_LOWER] = LAYOUT( \
   _______, _______ , _______, _______, _______, _______,                   _______,    KC_7,    KC_8,    KC_9, KC_ASTR, _______, \
@@ -42,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11, \
   _______, _______, KC_HOME,   KC_UP,  KC_END, KC_PGUP,                    _______, _______, _______,   KC_INS, KC_PSCR,  KC_F12, \
   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                    _______, _______, _______,  _______, KC_CAPS, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, KC_MPLY, _______, \
+  _______, KC_PIPE, _______, KC_GRV, _______, _______, _______, _______, _______, _______, _______,  _______, KC_MPLY, _______, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 
@@ -50,6 +60,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+  ),
+  [_SHORTCUTS] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, POOP,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX,   TFLIP,   SHRUG, SMILE_HEB, SMILE,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
   )
@@ -108,5 +125,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
     // set_timelog();
   }
+  if (record->event.pressed){
+
+    switch (keycode) {
+    case SMILE:
+        SEND_STRING(":)");
+        break;
+    case SMILE_HEB:
+        SEND_STRING(":(");
+        break;
+    case SHRUG:
+        //send_unicode_string("¯\\_(ツ)_/¯");
+        break;
+    case TFLIP:
+        //send_unicode_string("(╯°□°)╯︵ ┻━┻");
+        break;
+    case POOP:
+        //send_unicode_string("💩");
+        break;
+    }
+  }
   return true;
+}
+
+void matrix_init_user(void) {
+    set_unicode_input_mode(UC_WINC);
 }
