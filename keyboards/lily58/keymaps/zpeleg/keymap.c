b9cf6c300ec47c14c52c996c08d6ca925eae8fe3
Ziv Peleg <zivpeleg@gmail.com>
2021-01-06 20:48:30 +0200

    enable media keys and make language change key cross platform

--- keyboards/lily58/keymaps/zpeleg/keymap.c
+++ keyboards/lily58/keymaps/zpeleg/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,                   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
   KC_TAB,   KC_Q,                   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LALT,  KC_A,                   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LT(_SHORTCUTS, KC_LBRC),  LCTL_T(KC_RBRC),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT(KC_LALT), \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LT(_SHORTCUTS, KC_LBRC),  LCTL_T(KC_RBRC),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LWIN(KC_SPC), \
                  KC_LGUI, MO(_RAISE), KC_LCTRL, LSFT_T(KC_ENT), KC_SPC, KC_BSPC, MO(_LOWER), KC_DEL \
 ),
 [_LOWER] = LAYOUT( \
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11, \
   _______, _______, KC_HOME,   KC_UP,  KC_END, KC_PGUP,                    _______, _______, _______,   KC_INS, KC_PSCR,  KC_F12, \
   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                    _______, _______, _______,  _______, KC_CAPS, _______, \
-  _______, KC_PIPE, _______, KC_GRV, _______, _______, _______, _______, _______, _______, _______,  _______, KC_MPLY, _______, \
+  _______, KC_PIPE, _______, KC_GRV, _______, _______, _______, _______, _______, KC__MUTE, KC__VOLDOWN,  KC__VOLUP, KC_MPLY, _______, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 
